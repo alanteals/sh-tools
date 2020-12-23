2de133e11994cb5b25ec8469e75f7a878c5d3c84
Allan Aste <allanaste@icloud.com>
2020-12-23 07:38:57 +0100

    Add argument for the folder

--- gpo.sh
+++ gpo.sh
@@ -2,9 +2,10 @@
 
 # Run 'git pull origin' on the master branch  in 1st-level subfolders
 
-for d in ./*/ ; do 
+for d in $1/*/ ; do 
     cd "$d";
     if git rev-parse --is-inside-work-tree; then
          git checkout master && git pull origin;
     fi
+    cd ..;
 done
