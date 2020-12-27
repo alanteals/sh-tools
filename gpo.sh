c03793255168452c6894ae121a4fd061ee93d88c
Allan Aste <allanaste@icloud.com>
2020-12-27 13:55:23 +0100

    Show current directory

--- gpo.sh
+++ gpo.sh
@@ -1,9 +1,10 @@
-#!/bin/sh
+#!/bin/bash
 
 # Run 'git pull origin' on the master branch  in 1st-level subfolders
 
 for d in $1/*/ ; do 
     cd "$d";
+    echo -e "\n${PWD}";
     if git rev-parse --is-inside-work-tree; then
          git checkout master && git pull origin;
     fi
